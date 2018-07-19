0062ceaebec8a2369ce259a5d92170874cb6f45b
Breezewish <me@breeswish.org>
2018-07-19 15:22:31 +0800

    cpu_tikv

--- tracing_tools/perf/cpu.sh
+++ tracing_tools/perf/cpu.sh
@@ -2,12 +2,5 @@
 
 # Usage: sudo cpu.sh pid
 
-if [ "$#" -ne 1 ]; then
-  PID=`pgrep tikv-server | head -n 1`
-  echo "Note: PID is not specified. Capturing for tikv-server PID = $PID"
-else
-  PID=$1
-fi
-
-perf record -F 99 -p $PID --call-graph=dwarf sleep 60
-perf script | ./fold-tikv-threads-perf.pl | /opt/FlameGraph/stackcollapse-perf.pl | /opt/FlameGraph/flamegraph.pl > cpu.svg
+perf record -F 99 -p $1 -g -- sleep 60
+perf script | /opt/FlameGraph/stackcollapse-perf.pl | /opt/FlameGraph/flamegraph.pl > cpu.svg

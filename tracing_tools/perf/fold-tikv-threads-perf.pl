24a70a8207a012d95479455b0d6ff83bda1d048f
Breezewish <me@breeswish.org>
2018-07-23 13:13:50 +0800

    add more threads

--- tracing_tools/perf/fold-tikv-threads-perf.pl
+++ tracing_tools/perf/fold-tikv-threads-perf.pl
@@ -10,11 +10,29 @@ while (defined($_ = <>)) {
     my $command = $1;
     my $pid = $2;
     my $remain = $3;
-    $command =~ s/^grpc-server-.*$/grpc-server/;
-    $command =~ s/^cop-.*$/cop/;
-    $command =~ s/^raftstore-.*$/raftstore/;
+    # grpc server. e.g. grpc-server-0
+    $command =~ s/^grpc-server-\d*$/grpc-server/;
+
+    # coprocessor read pool. e.g. cop-high0, cop-normal0, cop-low0
+    $command =~ s/^cop-high\d*$/cop-high/;
+    $command =~ s/^cop-normal\d*$/cop-normal/;
+    $command =~ s/^cop-low\d*$/cop-low/;
+
+    # raftstore. e.g. raftstore-1
+    $command =~ s/^raftstore-\d*$/raftstore/;
+
+    # SST importer. e.g. sst-importer0
+    $command =~ s/^sst-importer\d*$/sst-importer/;
+
+    # storage read pool. e.g. store-read-high, store-read-norm, store-read-low0
     $command =~ s/^store-read-.*$/store-read/;
-    $command =~ s/^rocksdb:bg.*$/rocksdb:bg/;
+
+    # rocksdb. e.g. rocksdb:bg0
+    $command =~ s/^rocksdb:bg\d*$/rocksdb:bg/;
+
+    # snapshot sender. e.g. snap sender0
+    $command =~ s/^snap sender\d*$/snap sender/;
+
     print $command, " ", $pid, $remain, "\n";
   } else {
     # other
